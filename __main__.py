e0407cedbb928a35d525008c0c285a7fd7ce533f
aaron-price <coding.aaronp@gmail.com>
2018-12-04 04:25:29 +0000

    Start scaffolding web frontend

--- __main__.py
+++ __main__.py
@@ -3,11 +3,13 @@ import ui
 import install
 import getRepo
 import db
+import web
 
 if __name__ == "__main__":
     data = ui.getData()
     install.installAll()
     getRepo.getRepo(data)
     db.setupDB(data)
+    web.setupWeb(data)
     # Setup server
     # Setup web frontend
