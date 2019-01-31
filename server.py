0150409c095dec672baa20ce71433cbee67a7f88
Aaron Price <coding.aaronp@gmail.com>
2019-01-30 16:36:45 -0800

    Add trailing comments

--- server.py
+++ server.py
@@ -9,17 +9,17 @@ def setupServer(data):
 
     # Set the IP
     ip = data["ip"]
-    replaceLine(config_path, "web_url", "System.get_env(\"PROJECT_IP\")")
+    replaceLine(config_path, "web_url", "System.get_env(\"PROJECT_IP\"),")
     sudo("echo export PROJECT_IP=\"%s\" >> ~/.bash_profile" % (ip))
     
     # Set db url
-    db_url = "http://localhost:8529/_db/" + title + "/gql"
+    db_url = "\"http://localhost:8529/_db/" + title + "/gql\","
     replaceLine(config_path, "db_url", db_url)
 
     # Set secret key base
     secret = title + "_SECRET_KEY_BASE"
     sudo("echo export %s=\"$(mix phx.gen.secret 90)\" >> ~/.bash_profile" % (secret))
-    replaceLine(config_path, "secret_key_base", "System.get_env(\"%s\")" % (secret))
+    replaceLine(config_path, "secret_key_base", "System.get_env(\"%s\")," % (secret))
     
     # Prepare elixir
     os.chdir(server_path)
