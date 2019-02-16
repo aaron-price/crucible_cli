9f365e12e41d6ba1d96daf4d412062b43af5a0a8
Aaron Price <coding.aaronp@gmail.com>
2019-02-16 14:23:49 -0800

    Fix imports

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@
 from ui import multipleChoice
 from ssh_gen import sshGen
 from bash import bashSetup
+from https import setupHttps
 
 if __name__ == "__main__":
     goal = multipleChoice("""
@@ -9,7 +10,9 @@ if __name__ == "__main__":
 
     1. Generate an ssh key
     2. Set up bash/vim
+    3. Set up NGINX with https
     """, {
         "1": sshGen,
         "2": bashSetup
+        "3": setupHttps
     })
