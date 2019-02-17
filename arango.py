6b1380e460fe1ff3d1898646f0d7b581b1735773
Aaron Price <coding.aaronp@gmail.com>
2019-02-16 18:14:32 -0800

    Add arango secure install

--- arango.py
+++ arango.py
@@ -3,5 +3,8 @@ import ui
 
 def setupArango():
     sh("cd /etc/yum.repos.d/ && curl -OL https://download.arangodb.com/arangodb34/RPM/arangodb.repo")
-    y("arangodb3-3.4.2-1")
-    y("arangodb3-debuginfo-3.4.2-1")
+    y("arangodb3")
+    y("arangodb3-debuginfo")
+    print "Here, have a password"
+    print genpass()
+    sudo("arango-secure-installation")
