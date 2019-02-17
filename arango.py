2b323eab6c32bdabd4599f5702e5bb0a959de01e
Aaron Price <coding.aaronp@gmail.com>
2019-02-16 18:04:10 -0800

    Fix paths

--- arango.py
+++ arango.py
@@ -2,7 +2,6 @@ from sh import *
 import ui
 
 def setupArango():
-    sh("cd /etc/yum.repos.d/")
-    sh("curl -OL https://download.arangodb.com/arangodb34/RPM/arangodb.repo")
+    sh("cd /etc/yum.repos.d/ && curl -OL https://download.arangodb.com/arangodb34/RPM/arangodb.repo")
     y("arangodb3-3.4.2-1")
     y("arangodb3-debuginfo-3.4.2-1")
